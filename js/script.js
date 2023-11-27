46e73e4f42882039511b952d927a731843d6be3b
Mikel Apraiz <apraiz.mikel@gmail.com>
2023-11-27 22:25:27 +0100

    Entregable 1 sprint 6 terminado

--- js/script.js
+++ js/script.js
@@ -1,41 +1,44 @@
 const userInput = document.getElementById("userInput");
-
-
-let comparar = function (numeroUsuario) {
-
-    let numeroOrdenador = Math.floor(Math.random() * 3)
-    
-    console.log(numeroOrdenador)
-    
-    if (numeroUsuario == numeroOrdenador) {
-        resultado = "¡Has salvado el mundo!"
-    } else {
-        resultado = "La bomba ha estallado";
-    }
-    
-    }
+const restart = document.getElementById("restart");
+let numeroSeleccionado;
+let resultado
 
 let cuentaAtras = 6;
-setInterval(() => { 
-    if(cuentaAtras==0){
-        clearInterval();
-        result.innerHTML= resultado
-     }else
-       {
-        cuentaAtras=cuentaAtras-1; 
-        document.getElementById('countdown').innerHTML=cuentaAtras; 
-       }
-    }, 1000);
 
-userInput.addEventListener("click", function() {
-    const numeroSeleccionado = document.getElementById('userInput').value;
-    comparar(numeroSeleccionado);
+setInterval(() => {
+    if (cuentaAtras == 0) {
+        clearInterval();
+        
+    } else {
+        cuentaAtras = cuentaAtras - 1;
+        document.getElementById('countdown').innerHTML = `<p>¡Rápido! ¡Selecciona un número! Solo te quedan ${cuentaAtras}</p>`;
+    }
+}, 1000)
+
+const nombrePromesa1 = new Promise((resolve) => {
+    setTimeout(() => {
+        let numeroOrdenador = Math.floor(Math.random()*(3 - 1) + 1);
+        resolve(numeroOrdenador);
+    },6000);
+})
+
+nombrePromesa1.then ((numero) => {
+    if (numeroSeleccionado == numero) {
+        resultado = `<p>¡Has salvado el mundo!</p><p>Tu número ${numeroSeleccionado} es el mismo que el eligido por la bomba, ${numero}</p>`
+    } else {
+        resultado = `<p>¡La bomba ha estallado!</p><p>Tu número ${numeroSeleccionado} no es el mismo que el eligido por la bomba, ${numero}</p>`
+    }
+    result.innerHTML=  resultado
+})
+
+
+userInput.addEventListener("input", function(e) {
+    numeroSeleccionado = e.target.value;
 });
 
-
-
-
-
+restart.addEventListener("click", () => {
+    location.reload()
+} )
 
 
 
